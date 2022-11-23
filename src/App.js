381bc2d36bf0e37894359be50c46eb616a58e974
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-23 18:16:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "./firebase/firebase.init";
 
-function App() {
+const auth = getAuth(app);
+
+const App = () => {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMew7OylfGKG2gWzr56wSBQ3Xd3ZJ1pHs",
+  authDomain: "simple-firebase-authenti-47c9d.firebaseapp.com",
+  projectId: "simple-firebase-authenti-47c9d",
+  storageBucket: "simple-firebase-authenti-47c9d.appspot.com",
+  messagingSenderId: "133073217524",
+  appId: "1:133073217524:web:6c2b1ac9add9ddeaffbba1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
